2c67587b19d690042438d0bcde3090c4b6da7cad
weijiezhu0204 <weijiezhu0204@qq.com>
2019-06-22 15:05:11 +0800

    fix filter-panel bug

--- packages/table/src/filter-panel.vue
+++ packages/table/src/filter-panel.vue
@@ -4,200 +4,214 @@
       class="el-table-filter"
       v-if="multiple"
       v-clickoutside="handleOutsideClick"
-      v-show="showPopper">
+      v-show="showPopper"
+    >
       <div class="el-table-filter__content">
         <el-scrollbar wrap-class="el-table-filter__wrap">
           <el-checkbox-group class="el-table-filter__checkbox-group" v-model="filteredValue">
             <el-checkbox
               v-for="filter in filters"
               :key="filter.value"
-              :label="filter.value">{{ filter.text }}</el-checkbox>
+              :label="filter.value"
+            >{{ filter.text }}</el-checkbox>
           </el-checkbox-group>
         </el-scrollbar>
       </div>
       <div class="el-table-filter__bottom">
-        <button @click="handleConfirm"
+        <button
+          @click="handleConfirm"
           :class="{ 'is-disabled': filteredValue.length === 0 }"
-          :disabled="filteredValue.length === 0">{{ t('el.table.confirmFilter') }}</button>
+          :disabled="filteredValue.length === 0"
+        >{{ t('el.table.confirmFilter') }}</button>
         <button @click="handleReset">{{ t('el.table.resetFilter') }}</button>
       </div>
     </div>
-    <div
-      class="el-table-filter"
-      v-else
-      v-clickoutside="handleOutsideClick"
-      v-show="showPopper">
+    <div class="el-table-filter" v-else v-clickoutside="handleOutsideClick" v-show="showPopper">
       <ul class="el-table-filter__list">
-        <li class="el-table-filter__list-item"
-            :class="{ 'is-active': filterValue === undefined || filterValue === null }"
-            @click="handleSelect(null)">{{ t('el.table.clearFilter') }}</li>
-        <li class="el-table-filter__list-item"
-            v-for="filter in filters"
-            :label="filter.value"
-            :key="filter.value"
-            :class="{ 'is-active': isActive(filter) }"
-            @click="handleSelect(filter.value)" >{{ filter.text }}</li>
+        <li
+          class="el-table-filter__list-item"
+          :class="{ 'is-active': filterValue === undefined || filterValue === null }"
+          @click="handleSelect(null)"
+        >{{ t('el.table.clearFilter') }}</li>
+        <li
+          class="el-table-filter__list-item"
+          v-for="filter in filters"
+          :label="filter.value"
+          :key="filter.value"
+          :class="{ 'is-active': isActive(filter) }"
+          @click="handleSelect(filter.value)"
+        >{{ filter.text }}</li>
       </ul>
     </div>
   </transition>
 </template>
 
 <script type="text/babel">
-  import Popper from 'element-ui/src/utils/vue-popper';
-  import { PopupManager } from 'element-ui/src/utils/popup';
-  import Locale from 'element-ui/src/mixins/locale';
-  import Clickoutside from 'element-ui/src/utils/clickoutside';
-  import Dropdown from './dropdown';
-  import ElCheckbox from 'element-ui/packages/checkbox';
-  import ElCheckboxGroup from 'element-ui/packages/checkbox-group';
+import Popper from 'element-ui/src/utils/vue-popper';
+import { PopupManager } from 'element-ui/src/utils/popup';
+import Locale from 'element-ui/src/mixins/locale';
+import Clickoutside from 'element-ui/src/utils/clickoutside';
+import Dropdown from './dropdown';
+import ElCheckbox from 'element-ui/packages/checkbox';
+import ElCheckboxGroup from 'element-ui/packages/checkbox-group';
+import ElScrollbar from 'element-ui/packages/scrollbar';
 
-  export default {
-    name: 'ElTableFilterPanel',
+export default {
+  name: 'ElTableFilterPanel',
 
-    mixins: [Popper, Locale],
+  mixins: [Popper, Locale],
 
-    directives: {
-      Clickoutside
-    },
+  directives: {
+    Clickoutside
+  },
 
-    components: {
-      ElCheckbox,
-      ElCheckboxGroup
-    },
+  components: {
+    ElCheckbox,
+    ElCheckboxGroup,
+    ElScrollbar
+  },
 
-    props: {
-      placement: {
-        type: String,
-        default: 'bottom-end'
-      }
-    },
+  props: {
+    placement: {
+      type: String,
+      default: 'bottom-end'
+    }
+  },
 
-    customRender(h) {
-      return (<div class="el-table-filter">
-        <div class="el-table-filter__content">
-        </div>
+  customRender(h) {
+    return (
+      <div class="el-table-filter">
+        <div class="el-table-filter__content" />
         <div class="el-table-filter__bottom">
-          <button on-click={ this.handleConfirm }>{ this.t('el.table.confirmFilter') }</button>
-          <button on-click={ this.handleReset }>{ this.t('el.table.resetFilter') }</button>
+          <button on-click={this.handleConfirm}>
+            {this.t('el.table.confirmFilter')}
+          </button>
+          <button on-click={this.handleReset}>
+            {this.t('el.table.resetFilter')}
+          </button>
         </div>
-      </div>);
+      </div>
+    );
+  },
+
+  methods: {
+    isActive(filter) {
+      return filter.value === this.filterValue;
     },
 
-    methods: {
-      isActive(filter) {
-        return filter.value === this.filterValue;
-      },
+    handleOutsideClick() {
+      setTimeout(() => {
+        this.showPopper = false;
+      }, 16);
+    },
 
-      handleOutsideClick() {
-        setTimeout(() => {
-          this.showPopper = false;
-        }, 16);
-      },
+    handleConfirm() {
+      this.confirmFilter(this.filteredValue);
+      this.handleOutsideClick();
+    },
 
-      handleConfirm() {
+    handleReset() {
+      this.filteredValue = [];
+      this.confirmFilter(this.filteredValue);
+      this.handleOutsideClick();
+    },
+
+    handleSelect(filterValue) {
+      this.filterValue = filterValue;
+
+      if (typeof filterValue !== 'undefined' && filterValue !== null) {
         this.confirmFilter(this.filteredValue);
-        this.handleOutsideClick();
+      } else {
+        this.confirmFilter([]);
+      }
+
+      this.handleOutsideClick();
+    },
+
+    confirmFilter(filteredValue) {
+      this.table.store.commit('filterChange', {
+        column: this.column,
+        values: filteredValue
+      });
+      this.table.store.updateAllSelected();
+    }
+  },
+
+  data() {
+    return {
+      table: null,
+      cell: null,
+      column: null
+    };
+  },
+
+  computed: {
+    filters() {
+      return this.column && this.column.filters;
+    },
+
+    filterValue: {
+      get() {
+        return (this.column.filteredValue || [])[0];
       },
-
-      handleReset() {
-        this.filteredValue = [];
-        this.confirmFilter(this.filteredValue);
-        this.handleOutsideClick();
-      },
-
-      handleSelect(filterValue) {
-        this.filterValue = filterValue;
-
-        if ((typeof filterValue !== 'undefined') && (filterValue !== null)) {
-          this.confirmFilter(this.filteredValue);
-        } else {
-          this.confirmFilter([]);
+      set(value) {
+        if (this.filteredValue) {
+          if (typeof value !== 'undefined' && value !== null) {
+            this.filteredValue.splice(0, 1, value);
+          } else {
+            this.filteredValue.splice(0, 1);
+          }
         }
-
-        this.handleOutsideClick();
-      },
-
-      confirmFilter(filteredValue) {
-        this.table.store.commit('filterChange', {
-          column: this.column,
-          values: filteredValue
-        });
-        this.table.store.updateAllSelected();
       }
     },
 
-    data() {
-      return {
-        table: null,
-        cell: null,
-        column: null
-      };
-    },
-
-    computed: {
-      filters() {
-        return this.column && this.column.filters;
-      },
-
-      filterValue: {
-        get() {
-          return (this.column.filteredValue || [])[0];
-        },
-        set(value) {
-          if (this.filteredValue) {
-            if ((typeof value !== 'undefined') && (value !== null)) {
-              this.filteredValue.splice(0, 1, value);
-            } else {
-              this.filteredValue.splice(0, 1);
-            }
-          }
-        }
-      },
-
-      filteredValue: {
-        get() {
-          if (this.column) {
-            return this.column.filteredValue || [];
-          }
-          return [];
-        },
-        set(value) {
-          if (this.column) {
-            this.column.filteredValue = value;
-          }
-        }
-      },
-
-      multiple() {
+    filteredValue: {
+      get() {
         if (this.column) {
-          return this.column.filterMultiple;
+          return this.column.filteredValue || [];
         }
-        return true;
+        return [];
+      },
+      set(value) {
+        if (this.column) {
+          this.column.filteredValue = value;
+        }
       }
     },
 
-    mounted() {
-      this.popperElm = this.$el;
-      this.referenceElm = this.cell;
-      this.table.bodyWrapper.addEventListener('scroll', () => {
-        this.updatePopper();
-      });
+    multiple() {
+      if (this.column) {
+        return this.column.filterMultiple;
+      }
+      return true;
+    }
+  },
 
-      this.$watch('showPopper', (value) => {
-        if (this.column) this.column.filterOpened = value;
-        if (value) {
-          Dropdown.open(this);
-        } else {
-          Dropdown.close(this);
-        }
-      });
-    },
-    watch: {
-      showPopper(val) {
-        if (val === true && parseInt(this.popperJS._popper.style.zIndex, 10) < PopupManager.zIndex) {
-          this.popperJS._popper.style.zIndex = PopupManager.nextZIndex();
-        }
+  mounted() {
+    this.popperElm = this.$el;
+    this.referenceElm = this.cell;
+    this.table.bodyWrapper.addEventListener('scroll', () => {
+      this.updatePopper();
+    });
+
+    this.$watch('showPopper', value => {
+      if (this.column) this.column.filterOpened = value;
+      if (value) {
+        Dropdown.open(this);
+      } else {
+        Dropdown.close(this);
+      }
+    });
+  },
+  watch: {
+    showPopper(val) {
+      if (
+        val === true &&
+        parseInt(this.popperJS._popper.style.zIndex, 10) < PopupManager.zIndex
+      ) {
+        this.popperJS._popper.style.zIndex = PopupManager.nextZIndex();
       }
     }
-  };
+  }
+};
 </script>
